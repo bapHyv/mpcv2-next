2ad359f5ba12d2bc377939f15df186cc3fe69805
Baptiste <baptiste.hyver@gmail.com>
2025-01-21 21:25:47 +0100

    still WIP Figure out how to close a menu when click outside and click on the icon :tada:

--- app/components/ProfileHeader.tsx
+++ app/components/ProfileHeader.tsx
@@ -13,8 +13,9 @@ import { useAuth } from "../context/authContext";
 import Link from "next/link";
 import { useTranslations } from "next-intl";
 import Separator from "./Separator";
-import { useEffect, useRef, useState } from "react";
+import { MutableRefObject, useEffect, useRef, useState } from "react";
 import { usePathname } from "next/navigation";
+import clsx from "clsx";
 
 interface Base {
   text: string;
@@ -42,49 +43,50 @@ type ItemsProfile = WithHref | WithOnClick;
 export default function ProfileHeader({ locale }: { locale: string }) {
   const [isVisible, setIsVisible] = useState(false);
   const pathName = usePathname();
-  const ignoreNextClick = useRef(false);
-
-  console.log(ignoreNextClick);
+  const iconRef = useRef<HTMLDivElement | null>(null);
 
   useEffect(() => {
     setIsVisible(false);
   }, [pathName]);
 
-  const handleIconClick = () => {
-    ignoreNextClick.current = true;
-    setIsVisible((prev) => !prev);
-  };
-
   return (
     <div>
-      <div className="flex" onClick={handleIconClick}>
+      <div ref={iconRef} className="flex" onClick={() => setIsVisible((prev) => !prev)}>
         <span className="sr-only">Open user menu</span>
         <UserCircleIcon className="h-8 w-8 text-white" role="button" />
       </div>
       {isVisible && (
         <UserMenu
           locale={locale}
-          onClickOutside={() => {
-            if (ignoreNextClick.current) {
-              ignoreNextClick.current = false;
-              return;
-            }
-            setIsVisible(false);
-          }}
+          onClickOutside={() => setIsVisible(false)}
+          iconRef={iconRef}
         />
       )}
     </div>
   );
 }
 
-function UserMenu({ locale, onClickOutside }: { locale: string; onClickOutside: any }) {
+function UserMenu({
+  locale,
+  onClickOutside,
+  iconRef,
+}: {
+  locale: string;
+  onClickOutside: any;
+  iconRef: MutableRefObject<HTMLDivElement | null>;
+}) {
   const ref = useRef<HTMLDivElement | null>(null);
   const t = useTranslations("navbar");
   const { isSignedIn, logout } = useAuth();
 
   useEffect(() => {
     const handleClickOutside = (event: MouseEvent) => {
-      if (ref.current && !ref.current.contains(event.target as HTMLElement)) {
+      if (
+        ref.current &&
+        iconRef.current &&
+        !ref.current.contains(event.target as HTMLElement) &&
+        !iconRef.current.contains(event.target as HTMLElement)
+      ) {
         console.log("here");
         onClickOutside && onClickOutside();
       }
@@ -168,7 +170,7 @@ function UserMenu({ locale, onClickOutside }: { locale: string; onClickOutside: 
 
   const loggedOutItems = itemsAuth.map((e, i, a) => (
     <>
-      <div className={`flex items-center ${i === a.length - 1 ? "mb-3" : ""}`}>
+      <div className={clsx("flex items-center", { "mb-3": i === a.length - 1 })}>
         {e.icon}
         <Link href={`/${locale}/${e.href}`} className="p-2">
           {e.text}
