90c774a57b222a80b19b9f345bfdb785335560ea
Baptiste <baptiste.hyver@gmail.com>
2025-02-04 17:18:12 +0100

    Add articles to blog page for each articles, create a folder with page.tsx and error.tsx files in it; Add boilerplate code in each page.tsx and error.tsx files;

--- app/[locale]/blog/page.tsx
+++ app/[locale]/blog/page.tsx
@@ -1,62 +1,37 @@
-import Head from 'next/head';
+import data from "@/app/staticData/blog.json";
+import clsx from "clsx";
+import Image from "next/image";
+import Link from "next/link";
 
-export default function Delta8THC() {
+interface Params {
+  locale: string;
+}
+
+export default function Page({ params }: { params: Params }) {
+  const { locale } = params;
   return (
     <>
-      <Head>
-        <title>Delta-8-THC : effets et légalité en France | MonPlanCBD</title>
-        <meta
-          name="description"
-          content="Découvrez les effets du Delta-8-THC et sa légalité en France. Informations complètes sur ce cannabinoïde."
-        />
-      </Head>
-      <main className="max-w-3xl mx-auto p-6">
-        <article>
-          <h1 className="text-3xl text-green font-bold mb-4">Delta-8-THC : effets et légalité en France</h1>
-          <p className="text-gray-600 mb-6">Publié le 03/07/2022</p>
-
-          <section className="mb-6">
-            <h2 className="text-2xl text-green font-semibold mb-2">Qu’est-ce que le delta-8-THC ?</h2>
-            <p className="mb-4">
-              Le delta-8-THC, également connu sous le nom de delta-8-tétrahydrocannabinol, est un composé chimique dérivé du cannabis qui suscite un intérêt croissant en France. Contrairement au delta-9-THC, le delta-8-THC présente des propriétés légèrement différentes, offrant un effet psychotrope plus doux. Cette variation chimique suscite un débat constant sur sa légalité en France, où les lois sur les produits du cannabis évoluent.
-            </p>
-            <p className="mb-4">
-              L’utilisation du delta-8-THC est souvent comparée à celle du CBD, un autre composé du cannabis. Le CBD, ou cannabidiol, est non psychotrope et est souvent utilisé pour ses propriétés relaxantes et thérapeutiques. Les deux composés, le CBD et le delta-8-THC, sont issus de la même plante, mais leurs effets diffèrent.
-            </p>
-            <p>
-              La réglementation entourant le delta-8-THC en France est en constante évolution, et il est important de se tenir informé des dernières lois et réglementations concernant ce composé. La discussion sur ces sujets reste un sujet brûlant dans la communauté cannabis en France.
-            </p>
-          </section>
-
-          <section className="mb-6">
-            <h2 className="text-2xl text-green font-semibold mb-2">Quels sont les effets du delta-8-THC ?</h2>
-            <p className="mb-4">
-              Le delta-8 présente des effets similaires au delta-9-tétrahydrocannabinol mais ceux-ci sont globalement plus légers. Les consommateurs évoquent un sentiment d’euphorie, une sensation de détente ou encore le soulagement de certaines douleurs. L’effet de “high” du delta-8 THC est plus faible que celui procuré par le delta-9-THC.
-            </p>
-            <p className="mb-4">
-              Les études scientifiques consacrées au delta-8-tétrahydrocannabinol sont encore très limitées. Les premiers travaux de recherche lui attribuent tout de même certaines propriétés thérapeutiques :
-            </p>
-            <ul className="list-disc list-inside mb-4">
-              <li>Inhibition de la croissance de tumeurs</li>
-              <li>Accompagnement de traitements en oncologie</li>
-              <li>Stimulation de l’appétit</li>
-            </ul>
-            <p>
-              Le nombre d’études scientifiques et les moyens attribués à la recherche sur les cannabinoïdes étant très limités, il est important de préciser que les effets thérapeutiques et les risques potentiels liés à la consommation de delta-8-THC sont encore certainement méconnus pour la plupart.
-            </p>
-          </section>
-
-          <section className="mb-6">
-            <h2 className="text-2xl text-green font-semibold mb-2">Le delta-8-THC est-il légal en France ?</h2>
-            <p>
-              Selon la loi française, la vente et la consommation de tous les THC sont interdites (delta-8, delta-9, delta-10…). Les tétrahydrocannabinols sont d’ailleurs inscrits sur la liste des stupéfiants. Face au manque de connaissances et d’études scientifiques, le delta-8-THC est donc interdit en France.
-            </p>
-            <p>
-              Pour rappel, en France, seul un taux de 0,3% de THC est autorisé dans les fleurs de CBD et les produits à base de chanvre comme l’huile de CBD.
-            </p>
-          </section>
-        </article>
-      </main>
+      <h1 className="mt-4 xl:mt-6 text-center uppercase text-green text-3xl font-medium">Blog</h1>
+      <div className="flex flex-wrap justify-center gap-2 my-4 px-2">
+        {data.map((e, i) => (
+          <Link
+            key={`${e.alt}-${i}`}
+            href={`/blog/${e.href}`}
+            className={clsx(
+              "w-full flex flex-col items-center gap-1 border border-neutral-300 rounded-md p-2 shadow-md",
+              "sm:w-1/3",
+              "lg:w-1/4",
+              "xl:w-1/5"
+            )}
+          >
+            <h2 className="text-center">{e.title}</h2>
+            <div className="w-36 h-36">
+              <Image src={e.src} alt={e.alt} width={144} height={144} className="rounded-full object-cover" />
+            </div>
+            <span className="text-xs text-neutral-400">Published: {e.published}</span>
+          </Link>
+        ))}
+      </div>
     </>
   );
 }
