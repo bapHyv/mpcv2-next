bd04c186760c8106fc21d293c00f1b5dbc19fd34
Baptiste <baptiste.hyver@gmail.com>
2025-02-05 17:08:48 +0100

    :construction: WIP :construction: Initiate blog articles migration;

--- app/[locale]/blog/aux-origines-du-chanvre/page.tsx
+++ app/[locale]/blog/aux-origines-du-chanvre/page.tsx
@@ -1,3 +1,62 @@
+import Image from "next/image";
+
 export default function Page() {
-    return (<div></div>)
+  return (
+    <article>
+      <h1>Aux origines du chanvre</h1>
+      <p>
+        Le cannabis, ou <i>Cannabis Sativa L</i>., est un genre de plante à{" "}
+        <a href="https://monplancbd.fr" target="_blank" rel="noopener">
+          fleurs
+        </a>{" "}
+        qui pousse nativement dans de nombreuses parties du monde, en particulier dans les zones tropicales et humides, ainsi que dans les régions
+        montagneuses. C’est une plante extrêmement polyvalente qui, bien que illégale dans la majeure partie du monde aujourd’hui, a servi de culture
+        importante tout au long de l’histoire humaine.
+      </p>
+      <Image src="https://www.monplancbd.fr/wp-content/uploads/2020/09/8960257.jpeg" alt="Cbd chanvre" width="800" height="418" />
+      <figcaption>Une plantation de chanvre en Suisse. [Gian Ehrenzeller - Keystone]</figcaption>
+      <p>
+        Les premières mentions de l’usage du chanvre remontent à environ 2 700 avant Jésus-Christ. dans un classique de la littérature chinoise écrit
+        par l’empereur Shen Nong. Les chinois utilisaient le cannabis pour produire des fibres solides et résistantes utilisées dans la fabrication de
+        vêtements et de cordes. Ils l&apos;utilisent aussi comme médicament pour traiter de nombreuses pathologies allant des rhumatismes aux crampes
+        menstruelles.
+      </p>
+      <p>
+        Des fragments de fibres de chanvre ont également été retrouvés dans d’anciennes chambres funéraires chinoises datant d’environ 1 000 ans avant
+        Jésus-Christ. D’autres preuves archéologiques suggèrent que la culture et la consommation du cannabis en Chine remontent à 10 000 ans avant
+        notre ère, ce qui montre clairement que le cannabis et le chanvre sont depuis longtemps d’importantes cultures industrielles, malgré la
+        prohibition récente. Son usage s’est répandu très tôt dans le monde entier.
+      </p>
+      <Image
+        src="https://www.monplancbd.fr/wp-content/uploads/2020/09/cannabis-chine-utilisation-medicinale-fhf-2019-2.jpg"
+        alt="Cbd chanvre"
+        width="900"
+        height="350"
+      />
+      <figcaption>Illustration chinoise - effet du Cannabis Sativa L sur le corps humain</figcaption>
+      <p>
+        En Europe du Nord, les premières indications sur l’usage du cannabis à des fins thérapeutiques datent de plus de 1000 ans. Elles se rapportent
+        surtout aux graines de chanvre (ou chènevis) et à des préparations prescrites en usage externe (fumigation ou cataplasme). Les herboristes du
+        Moyen Âge faisaient la différence entre le chanvre engraissé (cultivé ou sativa), utilisé par exemple contre la toux et la jaunisse, et le
+        chanvre bâtard (naturel ou Ruderalis), appliqué empiriquement contre les nodosités et les tumeurs.
+      </p>
+      <Image
+        src="https://www.monplancbd.fr/wp-content/uploads/2020/09/marijuana_leaf_4-424x500-1-254x300.png"
+        alt="Cbd chanvre"
+        width="254"
+        height="300"
+      />
+      <figcaption>Illustration de Vienne datant du VIème Siècle du Cannabis sativa L</figcaption>
+      <p>
+        Massilia, l’actuelle Marseille, exportait dès le septième siècle avant notre ère des cordages de chanvre. Le nom de Cannebière qui a la même
+        racine que cannabis rappelle l’importance de cette plante dans l’économie locale.
+      </p>
+      <aside>
+        <p>
+          Sources: <cite>Cibdol</cite>, <cite>RTS</cite>, <cite>OSC</cite>, <cite>Zamnesia</cite>, <cite>NORML</cite>,{" "}
+          <cite>French Hemp Factory</cite>
+        </p>
+      </aside>
+    </article>
+  );
 }
--- app/[locale]/blog/delta-8-thc-effet-legalite-france/page.tsx
+++ app/[locale]/blog/delta-8-thc-effet-legalite-france/page.tsx
@@ -1,3 +1,58 @@
 export default function Page() {
-    return (<div></div>)
+  return (
+    <article>
+      <h1>H1: Delta-8-THC : effets et légalité en France</h1>
+      <p>
+        Il est de plus en plus courant de rencontrer des produits contenant du <strong>delta-8-THC</strong>. Cette molécule est-elle sans risque ?{" "}
+        <strong>Que dit la loi en France sur la consommation de delta-8 </strong>ou de delta-9 ? Dans cet article, nous vous présentons ce
+        cannabinoïde naturellement présent dans le chanvre, <strong>ses effets sur l’organisme</strong> et{" "}
+        <strong>ses potentielles propriétés thérapeutiques</strong>.
+      </p>
+      <h2>H2: Qu’est-ce que le delta-8-THC ?</h2>
+      <p>
+        Le delta-8-THC, également connu sous le nom de delta-8-tétrahydrocannabinol, est un composé chimique dérivé du cannabis qui suscite un intérêt
+        croissant en France. Contrairement au delta-9-THC, le delta-8-THC présente des propriétés légèrement différentes, offrant un effet psychotrope
+        plus doux. Cette variation chimique suscite un débat constant sur sa légalité en France, où les lois sur les produits du cannabis évoluent.
+      </p>
+      <p>
+        Lorsque vous entendez parler du <strong>THC</strong>, il est généralement question du <strong>delta-9-tétrahydrocannabinol </strong>
+        (delta-9-THC, delta-9), un cannabinoïde connu pour ses effets psychotropes. Le <strong>delta-8-tétrahydrocannabinol</strong> (delta-8-THC,
+        delta-8) est en fait un <strong>autre type de THC</strong> présentant quelques différences par rapport à son cousin plus connu. Comme tous les
+        cannabinoïdes, le delta-8 agit via notre <a href="/blog/cbd-et-systeme-endocannabinoide">système endocannabinoïde</a>.
+      </p>
+      <h2>Quels sont les effets du delta-8-THC ?</h2>
+      <ul>
+        <li>
+          <strong>Inhibition de la croissance de tumeurs</strong> :{" "}
+        </li>
+        <li>
+          <strong>Accompagnement de traitements en oncologie</strong> : en 1995,{" "}
+          <a href="https://pubmed.ncbi.nlm.nih.gov/7776837/" rel="nofollow noopener" target="_blank">
+            une étude
+          </a>{" "}
+          dirigée par le Professeur Mechoulam a permis de démontrer des propriétés antiémétiques (contre les nausées et les vomissements) chez des
+          enfants atteints de tumeurs cancéreuses.
+        </li>
+        <li>
+          <strong>Stimulation de l’appétit</strong> :{" "}
+          <a href="https://pubmed.ncbi.nlm.nih.gov/15099912/" rel="nofollow noopener" target="_blank">
+            une étude menée en 2004
+          </a>{" "}
+          (réalisée sur des souris) a démontré qu’une faible dose de delta-8-tétrahydrocannabinol stimulait davantage l’appétit que le
+          delta-9-tétrahydrocannabinol.
+        </li>
+      </ul>
+      <h3>H3: Le delta-8-THC est-il légal en France ?</h3>
+      <p>
+        Selon la loi française, <strong>la vente et la consommation de tous les THC sont interdites</strong> (delta-8, delta-9, delta-10…). Les
+        tétrahydrocannabinols sont d’ailleurs inscrits sur la liste des stupéfiants. Face au manque de connaissances et d’études scientifiques, le
+        delta-8-THC est donc interdit en France.
+      </p>
+      <p>
+        Pour rappel, en France, seul <strong>un taux de 0,3% de THC est autorisé</strong> dans les{" "}
+        <a href="https://www.monplancbd.fr/fleurs-cbd/">fleurs de CBD</a> et les produits à base de chanvre comme l’
+        <a href="https://www.monplancbd.fr/huiles-cbd/">huile de CBD</a>.
+      </p>
+    </article>
+  );
 }
